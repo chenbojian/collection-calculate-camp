ff4df0c5bca17a8bb89954f1f7465458892962ba
chenbojian <chenbojian@foxmail.com>
2015-07-01 23:32:37 +0800

    flaten passed

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -3,6 +3,17 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+  var arr = [];
+  collection.forEach(function (value) {
+    if(Array.isArray(value)){
+      value.forEach(function (value_2) {
+        arr.push(value_2);
+      })
+    }else{
+      arr.push(value);
+    }
+  });
+  return arr;
 }
 
 module.exports = double_to_one;
--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -3,6 +3,21 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+  var arr = [];
+  collection.forEach(function (value) {
+    if (Array.isArray(value)) {
+      value.forEach(function (value_2) {
+        if (arr.indexOf(value_2) == -1) {
+          arr.push(value_2);
+        }
+      })
+    } else {
+      if (arr.indexOf(value) == -1) {
+        arr.push(value);
+      }
+    }
+  });
+  return arr;
 }
 
 module.exports = double_to_one;
